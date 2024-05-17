37d70247641b0278347d068db4c26b87fdf28bcd
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-18 00:36:52 +0200

    [+] added the file with all the structs

---
+++ include/structs.h
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2024
+** structs.h
+** File description:
+** All structs for the project
+*/
+
+#pragma once
+    #include <SFML/Graphics.h>
+    #include <SFML/Graphics/Types.h>
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <stdbool.h>
+    #define MOVE_SPEED 7
+
+typedef struct player_s {
+    sfSprite *runner;
+    sfTexture *run_tex;
+    bool is_reversed;
+    sfVector2f pos;
+    bool is_jumping;
+} player_t;
+
+typedef struct flame_s {
+    player_t *player;
+    sfRenderWindow *game_win;
+} flame_t ;
