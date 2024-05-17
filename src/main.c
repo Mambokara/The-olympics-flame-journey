2b3701abeac4666c218912dcd24bc7a08473aa60
Thomas <thomas.richalet@epitech.eu>
2024-05-18 00:54:05 +0200

    [+] Add init player and window
    	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -6,8 +6,40 @@
 */
 
 #include "../include/flame.h"
+#include <SFML/Graphics/RenderWindow.h>
+#include <SFML/System/Vector2.h>
+#include <SFML/Window/VideoMode.h>
+#include <SFML/Window/WindowBase.h>
+#include <stdbool.h>
+
+static player_t *init_player(void)
+{
+    player_t *player = malloc(sizeof(player_t));
+    sfVector2f position = {960, 540};
+
+    player->is_jumping = false;
+    player->is_reversed = false;
+    player->pos = position;
+    player->runner = NULL;
+    player->run_tex = NULL;
+    return player;
+}
+
+static flame_t *init_flame(void)
+{
+    flame_t *flame = malloc(sizeof(flame_t));
+    sfVideoMode mode = {1920, 1080, 32};
+
+    flame->player = init_player();
+    flame->game_win = sfRenderWindow_create(mode, "Flame",
+        sfClose | sfResize | sfDefaultStyle, NULL);
+    return flame;
+}
 
 int main(void)
 {
+    flame_t *flame = init_flame();
+
+    game_loop(flame);
     return 0;
 }
