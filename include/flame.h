510dca9832208678918f59f46746952a1f362f59
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-18 00:20:52 +0200

    [+] added the struct flame_t and added the player_t struct

--- include/flame.h
+++ include/flame.h
@@ -5,11 +5,23 @@
 ** All protos, include and struct for the project
 */
 
-#ifndef FLAME_H
-    #define FLAME_H
+#pragma once
     #include <SFML/Graphics.h>
     #include <SFML/Graphics/Types.h>
     #include <stdlib.h>
     #include <stdio.h>
+    #include <stdbool.h>
+    #define MOVE_SPEED 7
 
-#endif
+typedef struct player_s {
+    sfSprite *runner;
+    sfTexture *run_tex;
+    bool is_reversed;
+    sfVector2f pos;
+    bool is_jumping;
+} player_t;
+
+typedef struct flame_s {
+    player_t *player;
+    sfRenderWindow *game_win;
+} flame_t ;
