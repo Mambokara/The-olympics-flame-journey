4daadbb94d17b2d55c063ea8af3eb5c47d0866f7
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-18 00:33:36 +0200

    [+] added the file with all the functions

---
+++ include/functions.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** functions.h
+** File description:
+** All functions for the project
+*/
+
+
+#pragma once
+    #include <SFML/Graphics.h>
+    #include <SFML/Graphics/Types.h>
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <stdbool.h>
