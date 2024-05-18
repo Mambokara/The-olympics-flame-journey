fb97506591644454d631c2c147159dab14f4f950
milinmatteo <matteo.milin@epitech.eu>
2024-05-18 11:46:31 +0200

    [0] Fix coding style

--- src/init/init_pause_menu.c
+++ src/init/init_pause_menu.c
@@ -13,8 +13,10 @@ pause_menu_t *init_pause_menu(void)
     sfFont *font = sfFont_createFromFile("./assets/Pixellari.ttf");
 
     menu->is_displayed = 0;
-    menu->resume = create_text("Back to Game", font, (sfVector2f) {700, 700}, 80);
-    menu->main_menu = create_text("Go to Main Menu", font, (sfVector2f) {660, 400}, 80);
+    menu->resume = create_text("Back to Game", font,
+        (sfVector2f) {700, 700}, 80);
+    menu->main_menu = create_text("Go to Main Menu", font, (sfVector2f)
+        {660, 400}, 80);
     menu->quit = create_text("Leave Level", font, (sfVector2f) {740, 550}, 80);
     return menu;
 }
