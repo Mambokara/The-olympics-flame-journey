d06ae962f12bf42cec9ee7c177429501e19b02e0
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-18 02:16:27 +0200

    [+] added some init functions

--- src/main.c
+++ src/main.c
@@ -8,8 +8,29 @@
 #include "../include/functions.h"
 #include "../include/structs.h"
 #include <SFML/Graphics/RenderWindow.h>
+#include <SFML/Graphics/Sprite.h>
+#include <SFML/Graphics/Texture.h>
+#include <SFML/Graphics/Types.h>
+#include <SFML/Graphics/View.h>
 #include <SFML/System/Vector2.h>
 #include <stdbool.h>
+
+static sfTexture *get_texture(void)
+{
+    sfTexture *texture;
+
+    texture = sfTexture_createFromFile("assets/player_texture.png", NULL);
+    return texture;
+}
+
+static sfSprite *setup_sprite(sfTexture *texture, sfVector2f pos)
+{
+    sfSprite *sprite = sfSprite_create();
+
+    sfSprite_setTexture(sprite, texture, sfFalse);
+    sfSprite_setPosition(sprite, pos);
+    return sprite;
+}
 
 static player_t *init_player(void)
 {
@@ -19,9 +40,17 @@ static player_t *init_player(void)
     player->is_jumping = false;
     player->is_reversed = false;
     player->pos = position;
-    player->runner = NULL;
-    player->run_tex = NULL;
+    player->run_tex = get_texture();
+    player->runner = setup_sprite(player->run_tex, position);
     return player;
+}
+
+static sfView *init_view(player_t *player)
+{
+    sfView *view = sfView_create();
+
+    sfView_setCenter(view, sfSprite_getPosition(player->runner));
+    return view;
 }
 
 static flame_t *init_flame(void)
@@ -30,6 +59,7 @@ static flame_t *init_flame(void)
     sfVideoMode mode = {1920, 1080, 32};
 
     flame->player = init_player();
+    flame->view = init_view(flame->player);
     flame->game_win = sfRenderWindow_create(mode, "Flame",
         sfClose | sfResize | sfDefaultStyle, NULL);
     return flame;
