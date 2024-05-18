3940e3b0b0a56b21c1d475f218287d9b264fa3f7
Ewan <ewan.schaffhauser@gmail.com>
2024-05-18 06:28:04 +0200

    [+] check if a text is over ( change color )and if th button is pressed ( change outline thickness )

--- src/main_menu/button_over.c
+++ src/main_menu/button_over.c
@@ -8,8 +8,64 @@
 #include "../../include/functions.h"
 #include "../../include/structs.h"
 
+static sfBool is_over(sfText *text, sfVector2i mouse)
+{
+    const sfFloatRect frect = sfText_getGlobalBounds(text);
+
+    return (sfFloatRect_contains(&frect, mouse.x, mouse.y));
+}
+
+void is_pressed(flame_t *flame)
+{
+    sfVector2i mouse = sfMouse_getPositionRenderWindow(WINDOW);
+
+    if (is_over(flame->menu->credit, mouse)) {
+        sfText_setOutlineThickness(flame->menu->credit, 10);
+    } else
+        sfText_setOutlineThickness(flame->menu->credit, 5);
+    if (is_over(flame->menu->option, mouse)) {
+        sfText_setOutlineThickness(flame->menu->option, 10);
+    } else
+        sfText_setOutlineThickness(flame->menu->option, 5);
+    if (is_over(flame->menu->play, mouse)) {
+        sfText_setOutlineThickness(flame->menu->play, 10);
+    } else
+        sfText_setOutlineThickness(flame->menu->play, 5);
+    if (is_over(flame->menu->quit, mouse)) {
+        sfText_setOutlineThickness(flame->menu->quit, 10);
+    } else
+        sfText_setOutlineThickness(flame->menu->quit, 5);
+}
+
+static void change_color(sfText *text, bool condi)
+{
+    if (condi) {
+        sfText_setFillColor(text, sfBlack);
+        sfText_setOutlineColor(text, sfWhite);
+    } else {
+        sfText_setFillColor(text, sfWhite);
+        sfText_setOutlineColor(text, sfBlack);
+    }
+}
+
 void over_text(flame_t *flame)
 {
-    sfFloatRect bound = sfText_getGlobalBounds(flame->menu->credit);
     sfVector2i mouse = sfMouse_getPositionRenderWindow(WINDOW);
+
+    if (is_over(flame->menu->credit, mouse))
+        change_color(flame->menu->credit, true);
+    else
+        change_color(flame->menu->credit, false);
+    if (is_over(flame->menu->option, mouse))
+        change_color(flame->menu->option, true);
+    else
+        change_color(flame->menu->option, false);
+    if (is_over(flame->menu->play, mouse))
+        change_color(flame->menu->play, true);
+    else
+        change_color(flame->menu->play, false);
+    if (is_over(flame->menu->quit, mouse))
+        change_color(flame->menu->quit, true);
+    else
+        change_color(flame->menu->quit, false);
 }
