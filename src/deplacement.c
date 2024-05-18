f17b281d2340aedfdd367dfd6af93f318386502f
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-18 02:15:59 +0200

    [+] added the movement of the view with the player

--- src/deplacement.c
+++ src/deplacement.c
@@ -7,15 +7,18 @@
 
 #include "../include/structs.h"
 #include "../include/functions.h"
+#include <SFML/Graphics/View.h>
 
-int move_player(player_t *player, int side)
+int move_player(flame_t *flame, int side)
 {
     if (side == LEFT) {
-        sfSprite_move(player->runner, (sfVector2f){MOVE_SPEED, 0});
-        player->pos = sfSprite_getPosition(player->runner);
+        sfSprite_move(flame->player->runner, (sfVector2f){-MOVE_SPEED, 0});
+        flame->player->pos = sfSprite_getPosition(flame->player->runner);
+        sfView_setCenter(flame->view, flame->player->pos);
     } else {
-        sfSprite_move(player->runner, (sfVector2f){-MOVE_SPEED, 0});
-        player->pos = sfSprite_getPosition(player->runner);
+        sfSprite_move(flame->player->runner, (sfVector2f){MOVE_SPEED, 0});
+        flame->player->pos = sfSprite_getPosition(flame->player->runner);
+        sfView_setCenter(flame->view, flame->player->pos);
     }
     return 0;
 }
