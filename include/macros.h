a3a8124f8e0ed3a1acf910894f1c20e91a06eb9b
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-18 04:08:03 +0200

    [+] added an include for all the macros

---
+++ include/macros.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** macros.h
+** File description:
+** include for all the macros
+*/
+
+#include "structs.h"
+#pragma once
+
+    #define MOVE_SPEED 0.5
+    #define LEFT 0
+    #define RIGHT 1
+    #define WINDOW flame->game_win
+    #define PLAYER flame->player->runner
+    #define VIEW flame->view
+    #define MAP "assets/main_menu_background.jpg"
+    #define UNDERMAP "assets/back_map.png"
