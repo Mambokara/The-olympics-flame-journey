8213bffdc59f9bd5469914db4c14df30cc2d6501
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-18 05:25:52 +0200

    [+] added the gravity of the player

---
+++ src/gravity.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2024
+** gravity.h
+** File description:
+** file with the gravity functions
+*/
+
+#include "../include/structs.h"
+#include "../include/functions.h"
+#include <SFML/Graphics/Sprite.h>
+#include <SFML/Graphics/View.h>
+#include <SFML/System/Vector2.h>
+
+void check_gravity(flame_t *flame)
+{
+    sfVector2f center_view;
+    sfColor color;
+    const sfFloatRect rect = sfSprite_getGlobalBounds(flame->player->runner);
+
+    center_view = sfView_getCenter(flame->view);
+    color = sfImage_getPixel(flame->undermap, center_view.x,
+                    center_view.y + rect.height);
+    if (color.r == 255 && color.g == 0 && color.b == 0 &&
+        sfFloatRect_contains(&rect, center_view.x, center_view.y) == sfTrue){
+        return;
+    }
+    sfSprite_move(flame->player->runner, (sfVector2f){0, 1});
+    sfView_move(flame->view, (sfVector2f){0, 1});
+}
