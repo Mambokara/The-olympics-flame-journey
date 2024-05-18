a1aac64eddbfa73d5e4724381b6db04eafe2a532
Nathan Callegari <nathan.callegari@epitech.eu>
2024-05-18 11:22:13 +0200

    [+] create portal & stock the position & stock how much we create portal

--- src/utils/sprite_utils.c
+++ src/utils/sprite_utils.c
@@ -19,3 +19,20 @@ sfSprite *create_sprite(sfVector2f pos, char *file, sfVector2f scale)
     sfSprite_setTexture(sprite, texture, sfTrue);
     return sprite;
 }
+
+
+sfSprite *create_sprite_portal(portal_t *portal, sfVector2f pos, char *file, sfVector2f scale)
+{
+    static int counter = 0;
+    sfTexture *texture = sfTexture_createFromFile(file, NULL);
+    sfSprite *sprite = sfSprite_create();
+    sfVector2u coord = sfTexture_getSize(texture);
+
+    sfSprite_setScale(sprite, scale);
+    sfSprite_setPosition(sprite, pos);
+    sfSprite_setTexture(sprite, texture, sfTrue);
+    portal->portal_pos[counter]->pos = pos;
+    portal->nbr_portal += 1;
+    counter ++;
+    return sprite;
+}
