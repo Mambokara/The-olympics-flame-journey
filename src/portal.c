2b3ef6b8228eb266c7f21c3b42447375de96eeca
Nathan Callegari <nathan.callegari@epitech.eu>
2024-05-19 02:51:45 +0200

    [0] indentation

--- src/portal.c
+++ src/portal.c
@@ -17,14 +17,15 @@ portal_t *init_portal(void)
 
 static int coord_portal(player_t *player, portal_t *portal)
 {
-    for (int i = 0; i < portal->nbr_portal; i++)
-    if ((player->pos.x < portal->portal_pos[i]->pos.x + 10 || player->pos.x <
-        portal->portal_pos[i]->pos.x - 10) && (player->pos.y <
-        portal->portal_pos[i]->pos.y + 10 || player->pos.y <
-        portal->portal_pos[i]->pos.y - 10)) {
+    for (int i = 0; i < portal->nbr_portal; i++) {
+        if ((player->pos.x < portal->portal_pos[i]->pos.x + 10 || player->pos.x <
+            portal->portal_pos[i]->pos.x - 10) && (player->pos.y <
+            portal->portal_pos[i]->pos.y + 10 || player->pos.y <
+            portal->portal_pos[i]->pos.y - 10)) {
             return 1;
-    } else {
-        return 0;
+        } else {
+            return 0;
+        }
     }
 }
 
@@ -37,8 +38,7 @@ static void attribute_number_portal(portal_t *portal)
 
 static void teleport(player_t *player, portal_t *portal, int i)
 {
-    if (sfKeyboard_isKeyPressed(sfKeyF) == sfTrue &&
-        coord_portal(player, portal) == 1) {
+    if (coord_portal(player, portal) == 1) {
         player->pos.x = portal->portal_pos[i]->pos_tp.x;
         player->pos.y = portal->portal_pos[i]->pos_tp.y;
         sfSprite_setPosition(player->runner, player->pos);
