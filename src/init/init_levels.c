4e75485a07d1d00cbaa2f3a0ca4091c59b324f95
Mambokara <corentin.bergaentzle@epitech.eu>
2024-05-19 17:17:56 +0200

    [-] removed some prints

--- src/init/init_levels.c
+++ src/init/init_levels.c
@@ -13,12 +13,12 @@ level_t *init_level(int id)
     level_t *level = malloc(sizeof(level_t));
     char *file = strdup("./assets/levels/level");
 
-    write(1, "LKJFIULFHIUBF", 14);
     file = strcat(file, nbr_to_str(id));
     file = strcat(file, "_");
-    printf(file);
     level->background = create_sprite((sfVector2f){0, 0}, my_strcat(file, "background.png"), (sfVector2f){1, 1});
-    level->ground = create_sprite((sfVector2f){0, 0}, my_strcat(file, "map.png"), (sfVector2f){1, 1});
+    level->ground = create_sprite((sfVector2f){0, 0}, my_strcat(file, "collision.png"), (sfVector2f){1, 1});
+    // level->ground = create_sprite((sfVector2f){0, 0}, my_strcat(file, "map.png"), (sfVector2f){1, 1});
+    // level->ground = sfImage_createFromFile(my_strcat(file, "collision.png")); 
     level->undermap = sfImage_createFromFile(my_strcat(file, "collision.png")); 
     return level;  
 }
